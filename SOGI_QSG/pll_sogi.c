e70da4b049cf730412015c05eec974b8e261aa89
donghao2nanjing <dongyinhao@foxmail.com>
2018-09-07 19:49:00 +0800

    pll_sogi completed, not tested

---
+++ SOGI_QSG/pll_sogi.c
@@ -0,0 +1,31 @@
+#include "pll_sogi.h"
+
+void init_pll_sogi(pll_sogi_s * p, double kp, double ki, double update_period){
+    p->center_angle_freq = 50 ;
+    p->angle_freq = p->center_angle_freq * DOUBLE_PI_PLL_SOGI ;
+    p->update_period = update_period ;
+    p->loop_filter_ki = ki * p->update_period ;
+    p->loop_filter_kp = kp * p->update_period ;
+    p->phase_output = 0 ;
+    p->sogi_backward_integrator_output = 0 ;
+    p->sogi_forward_integrator_output = 0 ;
+    p->loop_filter_sum = 0 ;
+} 
+
+double calc_pll_sogi(pll_sogi_s * p, double pcc_voltage){
+    double error ;
+
+    error = pcc_voltage - p->sogi_forward_integrator_output ;
+    error -= p->sogi_backward_integrator_output * p->angle_freq ;
+    error *= p->angle_freq ;
+    p->sogi_forward_integrator_output += error * p->update_period;
+
+    p->sogi_backward_integrator_output += p->sogi_forward_integrator_output * p->update_period;
+
+    error = p->sogi_forward_integrator_output * cos(p->phase_output) + 
+        p->sogi_backward_integrator_output * sin(p->phase_output) ;
+    p->loop_filter_sum += error * p->loop_filter_ki ;
+    p->angle_freq = p->center_angle_freq + p->loop_filter_sum + p->loop_filter_kp * error ;
+    p->phase_output += p->angle_freq * p->update_period ;
+    return p->phase_output ;
+}
